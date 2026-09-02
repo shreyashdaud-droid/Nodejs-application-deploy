66cdbfee391c4ea446d7f3d3ad042438eba1fec4
Your Name <you@example.com>
2026-09-02 22:37:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'success', message: 'CI/CD Pipeline Running!' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started on port ${PORT}`);
+});
